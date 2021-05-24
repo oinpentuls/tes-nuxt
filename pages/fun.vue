b6fa4c7e60c46c8820269aa85873ef0538f90561
Ahmad Tohir <achmad.tohier@gmail.com>
2021-05-25 02:15:36 +0700

    landing page

--- pages/fun.vue
+++
@@ -1,3 +0,0 @@
-<template>
-  <h1>tetetoetoeokktoetok</h1>
-</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,79 +1,82 @@
 <template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        my-nuxt
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-        <nuxt-link to="/fun">tes</nuxt-link>
+  <div>
+    <nav class="flex justify-center fixed z-10 bg-white">
+      <div class="nav">
+        <img
+          class="py-2"
+          src="~/assets/ibunda.png"
+          width="120"
+          alt="logo ibunda"
+        />
       </div>
-    </div>
+    </nav>
+    <section class="w-full flex justify-center">
+      <div class="flex justify-center">
+        <div class="min-width">
+          <img
+            class="pt-12"
+            src="~/assets/mental_health.png"
+            alt="mental health"
+            width="420"
+          />
+          <div class="head text-gray-800">
+            <h1 class="font-bold text-2xl">Mental Health Test</h1>
+          </div>
+          <p class="font-bold text-sm mt-2 text-gray-500">
+            Mental Health Test membantu kamu mengetahui keyakinan dirimu dalam
+            menghadapi situasi untuk berhasil mencapai hasil yang kamu inginkan.
+          </p>
+
+          <div class="w-full flex justify-center py-12">
+            <button class="bg-blue-100 py-4 px-20 btn-start rounded">
+              Mulai
+            </button>
+          </div>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
 <script>
-export default {}
+export default {};
 </script>
 
 <style>
-/* Sample `apply` at-rules with Tailwind CSS
-.container {
-@apply min-h-screen flex justify-center items-center text-center mx-auto;
-}
-*/
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
+nav {
+  width: 100%;
+  box-shadow: 0 2px 4px -1px rgb(0 0 0 / 20%), 0 4px 5px 0 rgb(0 0 0 / 14%),
+    0 1px 10px 0 rgb(0 0 0 / 12%);
 }
 
-.title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
+.nav {
+  min-width: 480px;
+  width: 480px;
+}
+
+section {
+  min-width: 480px;
+  width: 480px;
+}
+
+.head::after {
+  content: "";
+  border-bottom: 4px solid #2b80a3;
+  width: 30%;
   display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
+  margin-top: 1rem;
 }
 
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
+.min-width {
+  min-width: 480px;
+  width: 480px;
+  padding: 0 1rem;
 }
 
-.links {
-  padding-top: 15px;
+.btn-start {
+  background: #2b80a3;
+  color: white;
+  text-align: center;
+  font-weight: bold;
 }
 </style>
